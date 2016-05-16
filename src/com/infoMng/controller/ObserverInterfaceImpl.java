ce032d2d0c5e7ea5659528d0c3d13fad23d32af0
mattia berretti <mattia.berretti@studio.unibo.it>
2016-05-16 19:08:15 +0200

    aggiunto inizio implementazione observer

---
+++ src/com/infoMng/controller/ObserverInterfaceImpl.java
@@ -0,0 +1,140 @@
+package com.infoMng.controller;
+
+import view.interfaces.ObserverInterface;
+import view.interfaces.ViewInterface;
+
+public class ObserverInterfaceImpl implements ObserverInterface {
+
+	private ViewInterface view;
+	
+	public ObserverInterfaceImpl(ViewInterface view) {
+		this.view = view;
+	}
+	
+	@Override
+	public void mostraClienti() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostraFornitori() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostraFatture() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostraMenu() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostraMagazzino() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostraRegistiIva() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostraReportVendite() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostraRiunioni() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostraScontrini() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostraDialogCampoObbligatorio() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostraDialogCerca() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostraDialogNuovo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostraDialogRegistrati() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostraDialogWrongPass() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostraDialogWrongUser() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void salvaUtente() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void salvaCliente() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void salvaFornitore() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void salvaFattura() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void salvaRiunione() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
