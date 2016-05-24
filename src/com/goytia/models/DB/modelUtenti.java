15cffe531a478ac5730552ad2a105042e828cdcf
4nju <4nju@localhost>
2016-05-24 17:02:51 +0200

    Added new methods for modelUtenti class: nuovoUtente, loginUtente, cambiaPassword, eliminaUtente

--- src/com/goytia/models/DB/modelUtenti.java
+++ src/com/goytia/models/DB/modelUtenti.java
@@ -65,5 +65,67 @@ public class modelUtenti {
 		return str != "" ? str : null;
 	}
 	
+	private static List<modelUtenti> elenco(){
+		MBQuery query = MBQuery.queryDaTabella("Utenti");
+		try {
+			return query.find().stream()
+					.map(e -> new modelUtenti(e))
+					.collect(Collectors.toList());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	public boolean nuovoUtente(String nome, String cognome, String mail, String username, String password){
+		modelUtenti nuovo = new modelUtenti(MBOggetto.oggettoDaTabella("Utenti"));
+		nuovo.setNome(nome);
+		nuovo.setCognome(cognome);
+		nuovo.setMail(mail);
+		nuovo.setUsername(username);
+		nuovo.setPassword(password);
+		return nuovo.oggetto.salva();
+	}
+	
+	public boolean loginUtenti(String username, String password){
+		return modelUtenti.elenco().stream()
+				.filter(e -> e.getUsername() == username &&  e.getPassword() == password)
+				.count() == 1;
+	}
+	
+	public boolean cambiaPassword(String nome, String cognome, String mail, String username, String password, String newPassword){
+		MBQuery query = MBQuery.queryDaTabella("Clienti");
+		query.whereEqualTo("Nome", ctrlStringa(nome));
+		query.whereEqualTo("Cognome", ctrlStringa(cognome));
+		query.whereEqualTo("Mail", ctrlStringa(mail));
+		query.whereEqualTo("Username", ctrlStringa(username));
+		query.whereEqualTo("Password", ctrlStringa(password));
+		try {
+			MBOggetto temp = query.find().get(0);
+			temp.setObjectValue("Password", newPassword);
+			return temp.salva();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean eliminaUtente(String nome, String cognome, String mail, String username, String password){
+		MBQuery query = MBQuery.queryDaTabella("Clienti");
+		query.whereEqualTo("Nome", ctrlStringa(nome));
+		query.whereEqualTo("Cognome", ctrlStringa(cognome));
+		query.whereEqualTo("Mail", ctrlStringa(mail));
+		query.whereEqualTo("Username", ctrlStringa(username));
+		query.whereEqualTo("Password", ctrlStringa(password));
+		try {
+			MBOggetto temp = query.find().get(0);
+			return temp.elimina();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
 	
 }
