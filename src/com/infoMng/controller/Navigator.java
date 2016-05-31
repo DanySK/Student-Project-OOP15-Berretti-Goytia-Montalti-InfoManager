11749e0b01a0069ef9bcb7a8a8a24685574dda6f
mattia berretti <mattia.berretti@studio.unibo.it>
2016-05-31 13:11:22 +0200

    add javadoc

--- src/com/infoMng/controller/Navigator.java
+++ src/com/infoMng/controller/Navigator.java
@@ -8,8 +8,18 @@ interface Navigator<T> {
 		return new ListOfObjectImpl<>(elementi);
 	}
 	
+	/**
+	 * Obtein the next item
+	 * @return
+	 * the next item in the list if the actual item is the last return the last item
+	 */
 	T avanti();
 
+	/**
+	 * Obtein the previus item
+	 * @return
+	 * the previus item in the list if the actual item is the first return the first item
+	 */
 	T indietro();
 
 	Boolean isPrimo();
